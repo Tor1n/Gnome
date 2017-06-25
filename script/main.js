69f5c3f1db8acdb586b9ca3996f03d57cb225a47
Tor1n <Tor1n@list.ru>
2017-06-25 22:36:03 +0300

    1 comm

---
+++ script/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+ $('.cascade').casadeLanding();
+ $('.blue').click();
+});
---
+++ script/page.js
@@ -0,0 +1,19 @@
+$('.gallery-list').hover(
+ function() {},
+ function()
+ {
+   $(this).find('li img').animate({opacity:1},250);
+ }
+);
+
+$('.gallery-list li').hover(
+ function()
+ {
+   $(this).siblings('li').find('img').css('opacity',0.5);
+   $(this).find('img').animate({opacity:1},250);
+ },
+ function()
+ {
+   $(this).find('img').css('opacity',1);
+ }
+);
